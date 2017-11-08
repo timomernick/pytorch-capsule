#fixbug: squash the wrong dimention give rise to the bad result.

capsule_layer.py
@@ -19,7 +19,7 @@ class ConvUnit(nn.Module):
                                out_channels=32,  # fixme constant
                                kernel_size=9,  # fixme constant
                                stride=2,
-                               bias=False)  # fixme constant
+                               bias=True)  # fixme constant
 
     def forward(self, x):
         return self.conv0(x)
@@ -49,9 +49,9 @@ class CapsuleLayer(nn.Module):
             self.units = [create_conv_unit(i) for i in range(self.unit_size)]
 
     @staticmethod
-    def squash(s):
+    def squash(s, dim=2):
         # This is equation 1 from the paper.
-        mag_sq = torch.sum(s**2, dim=2, keepdim=True)
+        mag_sq = torch.sum(s**2, dim, keepdim=True)
         mag = torch.sqrt(mag_sq)
         s = (mag_sq / (1.0 + mag_sq)) * (s / mag)
         return s
@@ -74,7 +74,7 @@ class CapsuleLayer(nn.Module):
         u = u.view(x.size(0), self.unit_size, -1)
 
         # Return squashed outputs.
-        return CapsuleLayer.squash(u)
+        return CapsuleLayer.squash(u, dim=1)
 
     def routing(self, x):
         batch_size = x.size(0)
@@ -85,7 +85,7 @@ class CapsuleLayer(nn.Module):
         # (batch, features, in_units) -> (batch, features, num_units, in_units, 1)
         x = torch.stack([x] * self.num_units, dim=2).unsqueeze(4)
 
-        # (batch, features, in_units, unit_size, num_units)
+        # (batch, features, num_units, unit_size, in_units)
         W = torch.cat([self.W] * batch_size, dim=0)
 
         # Transform inputs by weight matrix.
@@ -100,6 +100,7 @@ class CapsuleLayer(nn.Module):
         for iteration in range(num_iterations):
             # Convert routing logits to softmax.
             # (batch, features, num_units, 1, 1)
+            # fixme: seems apply wrong dimention here. but can't train the network if change it. weird.
             c_ij = F.softmax(b_ij)
             c_ij = torch.cat([c_ij] * batch_size, dim=0).unsqueeze(4)
 
@@ -108,6 +109,7 @@ class CapsuleLayer(nn.Module):
             s_j = (c_ij * u_hat).sum(dim=1, keepdim=True)
 
             # (batch_size, 1, num_units, unit_size, 1)
+            # fixme: seems apply wrong dimention here. but can't train the network if change it. weird.
             v_j = CapsuleLayer.squash(s_j)
 
             # (batch_size, features, num_units, unit_size, 1)

capsule_network.py
@@ -75,8 +75,8 @@ class CapsuleNetwork(nn.Module):
         zero = Variable(torch.zeros(1)).cuda()
         m_plus = 0.9
         m_minus = 0.1
-        max_l = torch.max(m_plus - v_mag, zero).view(batch_size, -1)
-        max_r = torch.max(v_mag - m_minus, zero).view(batch_size, -1)
+        max_l = torch.max(m_plus - v_mag, zero).view(batch_size, -1) ** 2
+        max_r = torch.max(v_mag - m_minus, zero).view(batch_size, -1) ** 2
 
         # This is equation 4 from the paper.
         loss_lambda = 0.5
@@ -118,11 +118,11 @@ class CapsuleNetwork(nn.Module):
             vutils.save_image(output_image, "reconstruction.png")
         self.reconstructed_image_count += 1
 
-        # The reconstruction loss is the mean squared difference between the input image and reconstructed image.
+        # The reconstruction loss is the sum squared difference between the input image and reconstructed image.
         # Multiplied by a small number so it doesn't dominate the margin (class) loss.
         error = (output - images).view(output.size(0), -1)
         error = error**2
-        error = torch.mean(error, dim=1) * 0.0005
+        error = torch.sum(error, dim=1) * 0.0005
 
         # Average over batch
         if size_average: